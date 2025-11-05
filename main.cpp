6ec1701cc6221fccc192b8404a1b82c482b5c846
Markus Stracke <mstracke@student.42berlin.de>
2025-11-05 11:40:59 +0100

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mstracke <mstracke@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/05 11:03:31 by mstracke          #+#    #+#             */
+/*   Updated: 2025/11/05 11:39:48 by mstracke         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <sys/socket.h> //for socket, bind, listen, accept
+#include <errno.h>
+#include <iostream>
+
+/**
+ * @param1: port to use
+ * @param2: pw required by any IRC client that tries to connect to this webserver
+ */
+int	main(int argc, char *argv[])
+{
+	int	fd_server = 0;
+	//TO-DO's:
+	//check number of arguments
+	//play around with required (& allowed) c-functions
+	//int socket(int domain, int type, int protocol);
+	//AF_INET == IPv4 | SOCK_STREAM = two-way connection-based byte streams | protocl number (if several)
+	fd_server = socket(AF_INET, SOCK_STREAM, 0);
+	if (fd_server < 0)
+	{
+		std::cerr << "Socket creation error" << std::endl;
+		return 1;
+	}
+	//int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+	//int listen(int sockfd, int backlog);
+	//int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
+}
