a004f38a28ce1bd97a9e53b3d3a57e4bb7f6b77d
zen_vega <ufogreeks@gmail.com>
2025-11-07 10:36:43 +0100

    fix: rollback clang cpp standard

--- src/Server/Server.hpp
+++ src/Server/Server.hpp
@@ -1,6 +1,6 @@
+#include <netinet/in.h>  //for socket, bind, listen, accept
+#include <sys/socket.h>  //sockaddr_in
 #include <iostream>
-#include <netinet/in.h> //for socket, bind, listen, accept
-#include <sys/socket.h> //sockaddr_in
 #include <vector>
 
 #ifndef SERVER_HPP
@@ -8,22 +8,22 @@
 
 class Server {
 
-	private:
-	  int			_port;
-	  int			_fd_server;
-	  struct sockaddr_in	_addr;
-	  std::string 		_pw;
-	  std::vector<struct pollfd> _poll_fds;
+ private:
+  int _port;
+  int _fd_server;
+  struct sockaddr_in _addr;
+  std::string _pw;
+  std::vector<struct pollfd> _poll_fds;
 
-	public:
-	  //OCF
-	  Server(int port, std::string &pw);
-	  Server(const Server &other);
-	  Server	&operator=(const Server &other);
-	  ~Server();
+ public:
+  // OCF
+  Server(int port, std::string& pw);
+  Server(const Server& other);
+  Server& operator=(const Server& other);
+  ~Server();
 
-	  //function to activate the IRC-Server (run the server-loop)
-	  int init();
+  // function to activate the IRC-Server (run the server-loop)
+  int init();
 };
 
-#endif // SERVER_HPP
+#endif  // SERVER_HPP
