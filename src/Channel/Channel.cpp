011b35077081c93ec9018e677014ba7992947cc4
mekundur <mertkunn@gmail.com>
2025-11-28 15:43:09 +0100

    @brief and TODO formatting is followed through. Cleaning is done. list of Channels added to Server class

--- src/Channel/Channel.cpp
+++ src/Channel/Channel.cpp
@@ -1,161 +1,119 @@
 #include "Channel.hpp"
+#include <iostream>
 #include <list>
 #include <string>
-#include <iostream>
-#include "../debug.hpp"
 #include "../Client/Client.hpp"
+#include "../debug.hpp"
 
-// Channel::Channel() : _name("default") {}
-Channel::Channel(std::string name)  
-    :   _name(name),
-        _topic("Default"),
-        _limit_mode(false),
-        _key_mode(false),
-        _invite_mode(false),
-        _topic_mode(false) {
-    DEBUG_PRINT("Channel is created");
-    print_channel_info();
+Channel::Channel(std::string name)
+    : _name(name),
+      _topic("Default"),
+      _limit_mode(false),
+      _key_mode(false),
+      _invite_mode(false),
+      _topic_mode(false) {
+  DEBUG_PRINT("Channel is created");
+  print_channel_info();
 }
 
-Channel::Channel(const Channel& other) 
-    :   _name(other._name),
-        _topic(other._topic),
-        _limit_mode(other._limit_mode),
-        _key_mode(other._key_mode),
-        _invite_mode(other._invite_mode),
-        _topic_mode(other._topic_mode) {}
+Channel::Channel(const Channel& other)
+    : _name(other._name),
+      _topic(other._topic),
+      _limit_mode(other._limit_mode),
+      _key_mode(other._key_mode),
+      _invite_mode(other._invite_mode),
+      _topic_mode(other._topic_mode) {}
 
 Channel& Channel::operator=(const Channel& other) {
-    Channel temp(other);
-    std::swap(*this, temp);
-    return (*this);
+  Channel temp(other);
+  std::swap(*this, temp);
+  return (*this);
 }
 
 Channel::~Channel() {
-    DEBUG_PRINT("Channel is destructed"); 
+  DEBUG_PRINT("Channel is destructed");
 }
 
 // Member management methods
-void    Channel::new_member(Client* _new) {
-    _members.push_back(_new);
-    DEBUG_PRINT("New member is added to the channel");
-    print_channel_info();
+void Channel::new_member(Client* _new) {
+  _members.push_back(_new);
+  DEBUG_PRINT("New member is added to the channel");
+  print_channel_info();
 }
 
-void    Channel::new_operator(Client* _new) {
-    _operators.push_back(_new);
-    DEBUG_PRINT("New operator is assigned for the channel");
-    print_channel_info();
+void Channel::new_operator(Client* _new) {
+  _operators.push_back(_new);
+  DEBUG_PRINT("New operator is assigned for the channel");
+  print_channel_info();
 }
 
-void    Channel::new_invited(Client* _new) {
-    _invited.push_back(_new);
-    DEBUG_PRINT("The user is invited to the channel");
-    print_channel_info();
+void Channel::new_invited(Client* _new) {
+  _invited.push_back(_new);
+  DEBUG_PRINT("The user is invited to the channel");
+  print_channel_info();
 }
 
 // SETTERS
-void    Channel::set_topic(std::string topic) {
-    _topic = topic;    
+void Channel::set_topic(std::string topic) {
+  _topic = topic;
 }
 
-void    Channel::set_user_limit(size_t limit) {
-    _user_limit = limit;
-    _limit_mode = true;
+void Channel::set_user_limit(size_t limit) {
+  _user_limit = limit;
+  _limit_mode = true;
 }
 
-void    Channel::set_invite_mode() {
-    if (_invite_mode)
-        _invite_mode = false;
-    else
-        _invite_mode = true;
+void Channel::set_invite_mode() {
+  if (_invite_mode)
+    _invite_mode = false;
+  else
+    _invite_mode = true;
 }
 
-void    Channel::set_key(std::string key) {
-    _key = key;
-    _key_mode = true;
+void Channel::set_key(std::string key) {
+  _key = key;
+  _key_mode = true;
 }
 
 // GETTERS
 std::string Channel::get_name() {
-    return(_name);
+  return (_name);
 }
 
 std::string Channel::get_topic() {
-    return(_topic);
+  return (_topic);
 }
 
 std::string Channel::get_key() {
-    return(_key);
+  return (_key);
 }
 
-bool    Channel::get_key_mode() {
-    return (_key_mode);
+bool Channel::get_key_mode() {
+  return (_key_mode);
 }
 
-bool    Channel::get_invite_mode() {
-    return (_invite_mode);
+bool Channel::get_invite_mode() {
+  return (_invite_mode);
 }
 
-bool    Channel::get_topic_mode() {
-    return (_topic_mode);
+bool Channel::get_topic_mode() {
+  return (_topic_mode);
 }
 
-bool    Channel::get_limit_mode() {
-    return (_limit_mode);
+bool Channel::get_limit_mode() {
+  return (_limit_mode);
 }
 
-int     Channel::get_user_limit() {
-    return (_user_limit);
+int Channel::get_user_limit() {
+  return (_user_limit);
 }
 
-size_t  Channel::get_members_size() {
-    return (_members.size());
+size_t Channel::get_members_size() {
+  return (_members.size());
 }
 
-void     Channel::get_members_fds() {
-        std::list<Client*>::iterator it = _members.begin();
-        for (; it != _members.end(); it++)
-            std::cout << *it << std::endl;
-        // return (_members);
+void Channel::print_channel_info() {
+  std::cout << get_name() << " [+i]:" << get_invite_mode()
+            << " [+t]:" << get_topic_mode() << " [+k]:" << get_key_mode()
+            << " [+l]:" << get_limit_mode() << std::endl;
 }
-
-// int     Channel::get_operators_fds() {
-
-// }
-
-// int     Channel::get_invited_fds() {
-
-// }
-
-
-void    Channel::print_channel_info() {
-        std::cout << get_name()   
-                    << " [+i]:" << get_invite_mode()
-                    << " [+t]:" << get_topic_mode()
-                    << " [+k]:" << get_key_mode()
-                    << " [+l]:" << get_limit_mode()
-                    << " Members:"<< get_members_size() << std::endl;  
-        get_members_fds();
-}
-
-// int main (void) {
-//     std::string key;
-//     std::string val;
-//     std::string name;
-//     std::vector<Channel>  my_channels;
-//     while (1) {
-//         if (std::cin.eof()) {
-//             break;
-//         }
-//         std::cin >> name;
-//         std::cout << "Channel_name: " << name << std::endl;
-//         std::cin >> key;
-//         std::cout << "Key: " << key << std::endl;
-//         std::cin >> val;
-//         std::cout << "Val: " << val << std::endl;
-//         Channel channel(name);
-//         my_channels.push_back(channel);
-//         // std::cin >> buf;
-//     }
-// }
--- src/Channel/Channel.hpp
+++ src/Channel/Channel.hpp
@@ -1,76 +1,78 @@
 #ifndef CHANNEL_HPP
-# define CHANNEL_HPP
+#define CHANNEL_HPP
 
-# include <string>
-# include <iostream>
-# include <list>
-# include "../Client/Client.hpp"
+#include <iostream>
+#include <list>
+#include <string>
+#include "../Client/Client.hpp"
 
-// Server will have a list of Channel objects
+/**
+ * @brief
+ * Server will have a list of Channel objects
+ ∗ KICK - Eject a client from the channel
+ ∗ INVITE - Invite a client to a channel
+ ∗ TOPIC - Change or view the channel topic
+ ∗ MODE - Change the channel’s mode:
+ * i: Set/remove Invite-only channel
+ * t: Set/remove the restrictions of the TOPIC command to channel
+ * k: Set/remove the channel key (password)
+ * o: Give/take channel operator privilege
+ * l: Set/remove the user limit to channel
+ *
+ * TODO 
+ * (1) a bitmask to check the modes 
+ * (2) if channel ıs mutable we'll need a setter function
+ */
 
-// ∗ KICK - Eject a client from the channel
-// ∗ INVITE - Invite a client to a channel
-// ∗ TOPIC - Change or view the channel topic
-// ∗ MODE - Change the channel’s mode:
-// · i: Set/remove Invite-only channel
-// · t: Set/remove the restrictions of the TOPIC command to channel
-// · k: Set/remove the channel key (password)
-// · o: Give/take channel operator privilege
-// · l: Set/remove the user limit to channel
-// a single integer to check the modes by bitwise operations maybe?
+class Channel {
 
-class   Channel {
+ private:
+  std::string _name;
+  std::string _topic;
+  std::string _key;
+  size_t _user_limit;
+  std::list<Client*> _members;
+  std::list<Client*> _operators;
+  std::list<Client*> _invited;
 
-    private:
-        std::string         _name;
-        std::string         _topic;
-        std::string         _key;
-        size_t              _user_limit;
-        std::list<Client*>  _members;
-        std::list<Client*>  _operators;
-        std::list<Client*>  _invited;
+  // booleans for Channel MODES:
+  bool _limit_mode;
+  bool _key_mode;
+  bool _invite_mode;
+  bool _topic_mode;
 
-        // booleans for Channel MODES:
-        bool                _limit_mode;     // +l mode: if channel has a limit
-        bool                _key_mode;       // +k mode: if channel has a password
-        bool                _invite_mode;   //  +i mode: only operators can add new members
-        bool                _topic_mode;    //  +t mode: only o
+ public:
+  Channel(std::string name);
+  Channel(const Channel& other);
+  Channel& operator=(const Channel& other);
+  ~Channel();
 
-    public:
-        // Channel();
-        Channel(std::string name);
-        Channel(const Channel& other);
-        Channel& operator=(const Channel& other);
-        ~Channel();
+  // Member management
+  void new_member(Client* _new);
+  void new_operator(Client* _new);
+  void new_invited(Client* _new);
 
-        // Member management methods
-        void        new_member(Client* _new);
-        void        new_operator(Client* _new);
-        void        new_invited(Client* _new);
+  // Getters
+  std::string get_name();
+  std::string get_topic();
+  std::string get_key();
+  bool get_key_mode();
+  bool get_invite_mode();
+  bool get_topic_mode();
+  bool get_limit_mode();
+  int get_user_limit();
+  size_t get_members_size();
+  void get_members_fds();
+  int get_operators_fds();
+  int get_invited_fds();
 
-        // getters
-        std::string get_name();
-        std::string get_topic();
-        std::string get_key();
-        bool        get_key_mode();
-        bool        get_invite_mode();
-        bool        get_topic_mode();
-        bool        get_limit_mode();
-        int         get_user_limit();
-        size_t      get_members_size();
-        void         get_members_fds();
-        int         get_operators_fds();
-        int         get_invited_fds();
+  // Setters
+  void set_topic(std::string topic);
+  void set_limit_mode(size_t limit);
+  void set_key(std::string key);
+  void set_invite_mode();
+  void set_user_limit(size_t limit);
 
-        // setters
-        // void        set_name();
-        void        set_topic(std::string topic);
-        // void        set_limit_mode(size_t limit);
-        void        set_key(std::string key);
-        void        set_invite_mode();
-        void        set_user_limit(size_t limit);
-
-        void        print_channel_info();
-
+  void print_channel_info();
 };
 #endif
