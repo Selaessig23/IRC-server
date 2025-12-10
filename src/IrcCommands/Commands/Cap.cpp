a1fc5c2c10a8af4f42c649e2af922a3b92d2f1ee
Markus Stracke <mstracke@student.42berlin.de>
2025-12-10 12:19:47 +0100

    style: add some cmments

--- src/IrcCommands/Commands/Cap.cpp
+++ src/IrcCommands/Commands/Cap.cpp
@@ -7,25 +7,33 @@
 #include "../IrcCommands.hpp"
 
 /**
- * @brief function to handle CAP LS negotiation, as some versions of clients like
- * irssi v1.2.3 are waiting for this specific response
+ * @brief function to handle CAP LS negotiation
+ *
+ * some versions of clients like irssi v1.2.3 are waiting for this specific response:
+ * if (cmd.parameters[0] != "END") {
+ *     send_message(base, cmd, ERR_INVALIDCAPCMD, true, NULL);
+ *     return (ERR_INVALIDCAPCMD);
+ * }
+ *
+ * TODO:
+ * (1) Think about which version to use
  */
 int IrcCommands::cap(Server& base, const struct cmd_obj& cmd) {
-  //   std::string response;
-  //   if (cmd.parameters[0] == "END") {
-  //     if (cmd.client->get_nick().empty())
-  //       response = "CAP *";
-  //     else
-  //       response = "CAP " + cmd.client->get_nick();
-  //     response += " END";
-  //     send_message(base, cmd, RPL_NONE, false, &response);
-  //   } else {
-  //     send_message(base, cmd, ERR_INVALIDCAPCMD, true, NULL);
-  //     return (ERR_INVALIDCAPCMD);
-  //   }
   if (cmd.parameters[0] != "END") {
     send_message(base, cmd, ERR_INVALIDCAPCMD, true, NULL);
     return (ERR_INVALIDCAPCMD);
+  }
+  std::string response;
+  if (cmd.parameters[0] != "END") {
+    if (cmd.client->get_nick().empty())
+      response = "CAP *";
+    else
+      response = "CAP " + cmd.client->get_nick();
+    response += " LS";
+    response += "\r\n";
+    cmd.client->add_client_out(response);
+    base.set_pollevent(cmd.client->get_client_fd(), POLLOUT);
+    return (0);
   }
   return (0);
 }
