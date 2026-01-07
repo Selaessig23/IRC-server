c3d5ab0b0a51fe3a13c8fa470ab8522fc9b54589
mekundur <mertkunn@gmail.com>
2026-01-07 16:19:18 +0100

    fix in PRIVMSG for creating receipents

--- src/IrcCommands/Commands/Privmsg.cpp
+++ src/IrcCommands/Commands/Privmsg.cpp
@@ -85,9 +85,7 @@ int IrcCommands::privmsg(Server& base, const struct cmd_obj& cmd) {
     end = it_para->find(',', start);
     start = end + 1;
   }
-  recipients.push_back(it_para->substr(start));
   it_para++;
-
   if (it_para == cmd.parameters.end() || it_para->empty()) {
     send_message(base, cmd, ERR_NOTEXTTOSEND, cmd.client, NULL);
     return (ERR_NOTEXTTOSEND);
